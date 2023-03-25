ce40601245254912639538f23cc6cc402c460820
Maggie <96947247+MaggieLOL@users.noreply.github.com>
2023-03-25 21:55:44 +0800

    Update minify.py

--- minify.py
+++ minify.py
@@ -2,4 +2,4 @@ import json
 with open("list.json","r",encoding="utf-8") as f:
   data=json.load(f)
 with open("list.min.json","w+",encoding="utf-8") as f:
-  json.dump(data,f)
+  json.dump(data,f, ensure_ascii=False)
